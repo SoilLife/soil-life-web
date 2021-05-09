1943b0638f1684f0212af8b4e0a3e2e5dccce53b
Billy Le <lebilly87@gmail.com>
2021-05-09 04:58:39 -0700

    billyle/#8: added link back to home on 404 page. ignored unused variables

--- components/atoms/card-six-f/index.tsx
+++ components/atoms/card-six-f/index.tsx
@@ -1,5 +1,12 @@
 import { CardSixFProps } from './card-six-f.interfaces';
 
-export function Card6F({ color, ctaText, icon, subtext, text, className }: CardSixFProps) {
+export function Card6F({
+  color: _color,
+  ctaText: _ctaText,
+  icon: _icon,
+  subtext: _subtext,
+  text: _text,
+  className,
+}: CardSixFProps) {
   return <div className={`${className}`}></div>;
 }
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,17 @@
 import DefaultLayout from 'layouts/default';
+import Link from 'next/link';
 
 export default function NotFoundPage() {
   return (
     <DefaultLayout>
-      <div className='container h-full flex-grow flex'>
-        <p className='max-w-2xl mx-auto text-center self-center text-5xl'>
+      <section className='container min-h-screen h-full flex flex-col items-center justify-center'>
+        <p className='max-w-2xl mx-auto text-center text-5xl h-full mb-8'>
           Oops. We couldn't find what you're looking for.
         </p>
-      </div>
+        <Link href='/'>
+          <a className='text-lg text-blue-500'>Go back home</a>
+        </Link>
+      </section>
     </DefaultLayout>
   );
 }
