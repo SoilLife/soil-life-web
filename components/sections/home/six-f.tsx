9460ef80c43b58f5dd8deec16740296156d14e51
Billy Le <lebilly87@gmail.com>
2021-06-02 18:32:04 +0800

    chore: upgrade dependencies and remove react-awesome-slider

--- components/sections/home/six-f.tsx
+++ components/sections/home/six-f.tsx
@@ -1,27 +1,24 @@
 import { Card6F } from 'components/atoms/card-six-f';
 import { slides } from 'data/six-f-slides';
 import { IKImage } from 'imagekitio-react';
-import Slider from 'react-awesome-slider';
 
 export function SixFSection() {
   return (
     <section className='relative h-screen overflow-hidden'>
-      <Slider fillParent mobileTouch className='h-full'>
-        {slides.map(({ photoUrl, ...slide }, index) => {
-          return (
-            <div key={index} className='max-h-full'>
-              <Card6F {...slide} />
-              <IKImage
-                key={index}
-                path={photoUrl}
-                loading='lazy'
-                lqip={{ active: true }}
-                className='absolute w-full top-56 left-0'
-              />
-            </div>
-          );
-        })}
-      </Slider>
+      {slides.map(({ photoUrl, ...slide }, index) => {
+        return (
+          <div key={index} className='max-h-full'>
+            <Card6F {...slide} />
+            <IKImage
+              key={index}
+              path={photoUrl}
+              loading='lazy'
+              lqip={{ active: true }}
+              className='absolute w-full top-56 left-0'
+            />
+          </div>
+        );
+      })}
     </section>
   );
 }
