ea845468073cc7c2399c409e977221026d9a2c87
Billy Le <lebilly87@gmail.com>
2021-07-11 03:23:54 -0700

    media-hub: updated for mobile. made search input longer

--- pages/media/index.tsx
+++ pages/media/index.tsx
@@ -215,8 +215,8 @@ export default function MediaPage() {
     <>
       <HomeHeader fullpageRef={{ current: null }} hideHeader={false} />
       <div className='my-28 space-y-16'>
-        <div className='container flex justify-between'>
-          <div className='flex items-center relative min-w-[320px]'>
+        <div className='container flex flex-col justify-between gap-4 xl:flex-row'>
+          <div className='flex items-center relative w-full sm:w-[480px] xl:w-[653px]'>
             <input
               type='text'
               className='border-none ring-1 ring-pink-500 rounded-full w-full placeholder-pink-300 outline-none focus:border-none focus:ring-2 focus:ring-pink-500'
@@ -228,12 +228,12 @@ export default function MediaPage() {
             </div>
           </div>
 
-          <div className='flex items-center space-x-10'>
+          <div className='flex items-center space-x-6 justify-between sm:space-x-10 sm:justify-start'>
             <p className='text-xl text-pink-500'>filters:</p>
-            <ul className='flex space-x-8'>
+            <ul className='flex space-x-6 sm:space-x-8'>
               <li>
                 <div
-                  className={`h-10 w-10 rounded-full cursor-pointer grid place-items-center hover:ring-2 hover:ring-pink-500 hover:bg-gradient-to-tr hover:from-pink-400 hover:to-transparent ${
+                  className={`h-6 w-6 sm:h-10 sm:w-10 rounded-full cursor-pointer grid place-items-center hover:ring-2 hover:ring-pink-500 hover:bg-gradient-to-tr hover:from-pink-400 hover:to-transparent ${
                     filters.includes('food') ? 'bg-pink-200' : ''
                   }`}
                   onClick={handleFilterChange('food')}
@@ -243,7 +243,7 @@ export default function MediaPage() {
               </li>
               <li>
                 <div
-                  className={`h-10 w-10 rounded-full cursor-pointer grid place-items-center hover:ring-2 hover:ring-yellow-500 hover:bg-gradient-to-tr hover:from-yellow-400 hover:to-transparent ${
+                  className={`h-6 w-6 sm:h-10 sm:w-10 rounded-full cursor-pointer grid place-items-center hover:ring-2 hover:ring-yellow-500 hover:bg-gradient-to-tr hover:from-yellow-400 hover:to-transparent ${
                     filters.includes('fiber') ? 'bg-yellow-200' : ''
                   }`}
                   onClick={handleFilterChange('fiber')}
@@ -253,7 +253,7 @@ export default function MediaPage() {
               </li>
               <li>
                 <div
-                  className={`h-10 w-10 rounded-full cursor-pointer grid place-items-center hover:ring-2 hover:ring-blue-500 hover:bg-gradient-to-tr hover:from-blue-400 hover:to-transparent ${
+                  className={`h-6 w-6 sm:h-10 sm:w-10 rounded-full cursor-pointer grid place-items-center hover:ring-2 hover:ring-blue-500 hover:bg-gradient-to-tr hover:from-blue-400 hover:to-transparent ${
                     filters.includes('filter') ? 'bg-blue-200' : ''
                   }`}
                   onClick={handleFilterChange('filter')}
@@ -263,7 +263,7 @@ export default function MediaPage() {
               </li>
               <li>
                 <div
-                  className={`h-10 w-10 rounded-full cursor-pointer grid place-items-center hover:ring-2 hover:ring-gray-500 hover:bg-gradient-to-tr hover:from-gray-400 hover:to-transparent ${
+                  className={`h-6 w-6 sm:h-10 sm:w-10 rounded-full cursor-pointer grid place-items-center hover:ring-2 hover:ring-gray-500 hover:bg-gradient-to-tr hover:from-gray-400 hover:to-transparent ${
                     filters.includes('foundations') ? 'bg-gray-200' : ''
                   }`}
                   onClick={handleFilterChange('foundations')}
@@ -273,7 +273,7 @@ export default function MediaPage() {
               </li>
               <li>
                 <div
-                  className={`h-10 w-10 rounded-full cursor-pointer grid place-items-center hover:ring-2 hover:ring-orange-500 hover:bg-gradient-to-tr hover:from-orange-400 hover:to-transparent ${
+                  className={`h-6 w-6 sm:h-10 sm:w-10 rounded-full cursor-pointer grid place-items-center hover:ring-2 hover:ring-orange-500 hover:bg-gradient-to-tr hover:from-orange-400 hover:to-transparent ${
                     filters.includes('farmaceuticals') ? 'bg-orange-200' : ''
                   }`}
                   onClick={handleFilterChange('farmaceuticals')}
@@ -283,7 +283,7 @@ export default function MediaPage() {
               </li>
               <li>
                 <div
-                  className={`h-10 w-10 rounded-full cursor-pointer grid place-items-center hover:ring-2 hover:ring-teal-500 hover:bg-gradient-to-tr hover:from-teal-400 hover:to-transparent ${
+                  className={`h-6 w-6 sm:h-10 sm:w-10 rounded-full cursor-pointer grid place-items-center hover:ring-2 hover:ring-teal-500 hover:bg-gradient-to-tr hover:from-teal-400 hover:to-transparent ${
                     filters.includes('fun') ? 'bg-teal-200' : ''
                   }`}
                   onClick={handleFilterChange('fun')}
