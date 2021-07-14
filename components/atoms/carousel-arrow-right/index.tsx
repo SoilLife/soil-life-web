a1edf14d096080ad6530fedaa467774a639c5d18
Billy Le <lebilly87@gmail.com>
2021-07-14 07:48:09 -0700

    media-hub: added reset search input icon. added carousel count to section headers. remove gradient backgrounds on filter buttons. adjusted carousel arrows to be smaller

--- components/atoms/carousel-arrow-right/index.tsx
+++ components/atoms/carousel-arrow-right/index.tsx
@@ -4,7 +4,7 @@ export function CarouselArrowRight({ className, ...props }: { onClick: () => voi
       className={`absolute top-1/2 transition-transform ease-out duration-300 transform -translate-y-1/2 z-10 cursor-pointer hover:scale-105 ${className}`}
       {...props}
     >
-      <img src='/images/right_arrow_pink_thick.svg' className='w-full h-20' />
+      <img src='/images/right_arrow_pink_thick.svg' className='w-full h-16' />
     </button>
   );
 }
