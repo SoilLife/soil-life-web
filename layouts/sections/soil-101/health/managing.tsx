fd829e089ccb1a7dfee7522deb76a464ea58ee34
Billy Le <lebilly87@gmail.com>
2021-09-29 09:37:09 -0700

    soil-101: updated soil health management section. updated text and spacing for soil health page

--- layouts/sections/soil-101/health/managing.tsx
+++ layouts/sections/soil-101/health/managing.tsx
@@ -1,23 +1,63 @@
+import { useState } from 'react';
+
 // components
 import { Text } from 'design-system/atoms';
 
 // assets
+import NoTillageSvg from 'public/images/soil-101/health/management_no_tillage.svg';
+import CoverCroppingSvg from 'public/images/soil-101/health/management_cover.svg';
+import HedgerowsSvg from 'public/images/soil-101/health/management_hedgerows.svg';
+import NutrientSvg from 'public/images/soil-101/health/management_nutrient.svg';
+import OrganicSvg from 'public/images/soil-101/health/management_organic.svg';
 
-const headers = ['no-tillage', 'cover cropping', 'hedgerows', 'nutrient management', 'organic amendments'];
+const headerSvgMap = {
+  'no-tillage': <NoTillageSvg />,
+  'cover cropping': <CoverCroppingSvg />,
+  hedgerows: <HedgerowsSvg />,
+  'nutrient management': <NutrientSvg />,
+  'organic amendments': <OrganicSvg />,
+};
+
+const headers: (keyof typeof headerSvgMap)[] = [
+  'no-tillage',
+  'cover cropping',
+  'hedgerows',
+  'nutrient management',
+  'organic amendments',
+];
 
 export const ManagingSection = (props: { assignRef: (el: null | HTMLDivElement) => void }) => {
+  const [activeHeader, setActiveHeader] = useState<keyof typeof headerSvgMap>('no-tillage');
+
   return (
-    <div ref={props.assignRef}>
-      <Text type='h1' weight='light' size='2xl' className='text-blue-500 mb-20'>
+    <div ref={props.assignRef} className='space-y-4 sm:space-y-8'>
+      <Text type='h1' weight='bold' size='4xl' className='text-blue-500 mb-8 sm:mb-20'>
         managing for soil health
       </Text>
-      <div className='flex flex-wrap items-center justify-between'>
-        {headers.map((header) => (
-          <Text key={header} type='p' weight='light' size='md'>
-            {header}
-          </Text>
-        ))}
+      <div className='overflow-x-auto overflow-y-hidden'>
+        <ul className='flex flex-nowrap items-center py-3 space-x-4 sm:space-x-8'>
+          {headers.map((header) => (
+            <li
+              key={header}
+              onClick={() => {
+                setActiveHeader(header);
+              }}
+            >
+              <Text
+                type='p'
+                weight='light'
+                size='sm'
+                className={`whitespace-nowrap ${
+                  header === activeHeader ? 'underline cursor-default' : 'cursor-pointer'
+                }`}
+              >
+                {header}
+              </Text>
+            </li>
+          ))}
+        </ul>
       </div>
+      {headerSvgMap[activeHeader]}
     </div>
   );
 };
--- layouts/sections/soil-101/health/risks.tsx
+++ layouts/sections/soil-101/health/risks.tsx
@@ -6,11 +6,11 @@ import RisksSvg from 'public/images/soil-101/health/risks.svg';
 
 export const RisksSection = (props: { assignRef: (el: null | HTMLDivElement) => void }) => {
   return (
-    <div ref={props.assignRef}>
-      <Text type='h1' weight='light' size='2xl' className='text-blue-500 mb-20'>
+    <div ref={props.assignRef} className='space-y-4 sm:space-y-8'>
+      <Text type='h1' weight='bold' size='3xl' className='text-blue-500 mb-8 sm:mb-20'>
         soils at risk
       </Text>
-      <Text type='p' weight='light' size='sm'>
+      <Text type='p' weight='light' size='sm' className='text-center'>
         a soccer field of soil is eroded every 5 seconds, an area the size of new york is sealed up every day, and 33%
         of the world’s soils are considered moderately to highly degraded. this poses an existential threat -- to our
         food supply and the global economy. there is a great opportunity to improve soil health and provide solutions to
