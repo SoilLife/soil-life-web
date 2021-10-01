41666c6a737a54d9195f031a5f4c9281764068f8
Billy Le <lebilly87@gmail.com>
2021-10-01 08:06:59 -0700

    billyle/#98: updated home page hero to break get connected into two lines. adjust line height so hero main text sits within the soil

--- layouts/sections/home/hero.tsx
+++ layouts/sections/home/hero.tsx
@@ -7,18 +7,21 @@ export function HeroSection() {
   return (
     <Section className='text-white'>
       <Image url='/Home_Page/natgeo5b_H4vFWWpPA.jpg' className='mt-10 object-cover object-top' />
-      <div className='absolute w-3/4 text-white transform translate-x-1/2 bottom-40 right-1/2 sm:w-1/2'>
-        <Text type='h1' weight='medium' size='5xl'>
+      <div className='absolute w-3/4 text-white transform translate-x-1/2 bottom-[15%] right-1/2 sm:w-1/2'>
+        <Text type='h1' weight='medium' size='5xl' style={{ lineHeight: 1 }}>
           we all share
         </Text>
-        <Text type='h1' weight='medium' size='5xl'>
+        <Text type='h1' weight='medium' size='5xl' style={{ lineHeight: 1 }}>
           common ground.
         </Text>
-        <div className='mt-4 h-5 sm:h-10 w-full bg-pink-500'></div>
+        <div className='mt-4 h-5 xl:h-6 2xl:h-10 w-full bg-pink-500'></div>
       </div>
-      <div className='absolute bottom-4 left-1/2 transform -translate-x-1/2'>
-        <Text type='p' weight='light' size='sm' className='mb-10'>
-          get connected
+      <div className='absolute bottom-4 left-1/2 transform -translate-x-1/2 text-center'>
+        <Text type='p' weight='light' size='sm'>
+          get
+        </Text>
+        <Text type='p' weight='light' size='sm' className='mb-1'>
+          connected
         </Text>
         <div className='flex justify-center'>
           <DownArrow height={20} />
