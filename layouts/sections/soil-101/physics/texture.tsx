8f65c61bba2b2494dc831afea51e9b101f922c8d
jlchiartas <40073963+jlchiartas@users.noreply.github.com>
2022-05-18 03:11:38 -0700

    Update texture.tsx

--- layouts/sections/soil-101/physics/texture.tsx
+++ layouts/sections/soil-101/physics/texture.tsx
@@ -106,8 +106,7 @@ export const TextureSection = (props: { assignRef: (el: null | HTMLDivElement) =
             <Text type='span' weight='bold' size='lg' style={{ color: '#FFCF01' }}>
               texture
             </Text>{' '}
-            of the soil -- how it feels in your hand, how water moves into and through it, how fertile it is, and much,
-            much more!
+            of the soil -- an inherent property that cannot be changed over human timescales. 
           </Text>
         </div>
       </div>
