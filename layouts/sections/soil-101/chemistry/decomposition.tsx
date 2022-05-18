d2ea0fe08525b7ef4d34a200c03318206b18cd26
jlchiartas <40073963+jlchiartas@users.noreply.github.com>
2022-05-18 05:38:19 -0700

    Update decomposition.tsx

--- layouts/sections/soil-101/chemistry/decomposition.tsx
+++ layouts/sections/soil-101/chemistry/decomposition.tsx
@@ -12,7 +12,12 @@ export const DecompositionSection = () => {
       <Text type='h1' weight='light' size='4xl' color='orange' className={styles['heading']}>
         decomposition
       </Text>
+      <Text type='p' weight='light' size='md' className={`text-center ${styles['p-48']}`}>
+        above ground, plant residues eventually fall to the surface; below ground roots slough off; 
+        and the soil food web eats and is eaten; cycling carbon and nutrients throughout the soil system.
+        </Text>
       <DecompositionSvg />
     </div>
+    
   );
 };
