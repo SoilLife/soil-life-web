9e5cdd80ba1c514501ce9d9a9d25765fa7ade0b9
jlchiartas <40073963+jlchiartas@users.noreply.github.com>
2022-05-26 14:47:35 -0700

    Update contact-us.tsx

--- layouts/sections/about-us/contact-us.tsx
+++ layouts/sections/about-us/contact-us.tsx
@@ -24,32 +24,21 @@ export function ContactUsSection() {
           <Text type='h1' weight='regular' size='2xl' color='pink' className='hidden mb-10 sm:block'>
             contact us
           </Text>
-          <div className='mb-4 md:mb-8 h-[54px]'>
-            <Input id='name' label='name' />
-          </div>
-          <div className='mb-4 md:mb-8 h-[54px]'>
-            <Input id='email' label='email' />
-          </div>
-
-          <div className='mb-4 md:mb-8 h-full'>
-            <textarea
-              id='message'
-              name='message'
-              placeholder='message'
-              className='p-2 w-full h-20 font-acre-thin sm:p-4 sm:h-80 sm:text-[30px]'
-              required
-            />
-          </div>
-
-          <div className='flex justify-end space-x-4'>
-            <button type='reset' className='px-8 py-2 text-white bg-gray-500'>
-              Reset
-            </button>
-            <button type='submit' className='px-8 py-2 text-white bg-pink-500'>
-              Send
-            </button>
-          </div>
-        </form>
+                <Text type='p' weight='light' size='sm'>
+                  e-mail: jessica@soillife.org
+                </Text>
+              </div>
+        
+        <div className='px-10 absolute bottom-10 w-full sm:bottom-auto sm:top-1/2 sm:max-w-3xl sm:transform sm:-translate-y-1/2'>
+        <form className='rounded-lg p-4 bg-white shadow-lg sm:p-10' onSubmit={handleSubmit}>
+          <Text type='h1' weight='regular' size='2xl' color='pink' className='hidden mb-10 sm:block'>
+            credits
+          </Text>
+          <Text type='p' weight='light' size='sm'>
+            The Nature and Properties of Soil - Nail Brady & Ray Weil      
+            For the Love of Soil - Karen Vaughn & Yamina Pressler
+            The Nito Project
+                </Text>
       </div>
     </Section>
   );
