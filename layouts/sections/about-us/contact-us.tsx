ef5951c5739655b9d700cfb5fe60759480023fd2
jlchiartas <40073963+jlchiartas@users.noreply.github.com>
2022-05-28 17:39:18 -0700

    Update contact-us.tsx

--- layouts/sections/about-us/contact-us.tsx
+++ layouts/sections/about-us/contact-us.tsx
@@ -76,6 +76,7 @@ export function ContactUsSection() {
                   All other photos were licensed from Adobe Stock, sourced under creative commons, or is protected under fair use, as this website is 'not-for-profit' and intended for educational purposes only. 
               </Paragraph>
                   </Text>{' '}
+               </Section>
             </div>
           </div>
         </div>
