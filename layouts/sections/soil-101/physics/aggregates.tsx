20f4a4850e9dc3191a288c3667aced5f6396f78b
jlchiartas <40073963+jlchiartas@users.noreply.github.com>
2022-05-30 16:28:21 -0700

    Update aggregates.tsx

--- layouts/sections/soil-101/physics/aggregates.tsx
+++ layouts/sections/soil-101/physics/aggregates.tsx
@@ -61,7 +61,7 @@ export const AggregatesSection = (props: { assignRef: (el: null | HTMLDivElement
           color='yellow'
           className={`${styles['anchor']} ${styles['heading']}`}
         >
-          aggregates
+          aggregation
         </Text>
         <AggregationSvg />
       </div>
