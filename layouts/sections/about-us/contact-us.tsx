6d74d27630b25d2494fe0e0b6a4fa654ec13bfa0
jlchiartas <40073963+jlchiartas@users.noreply.github.com>
2022-05-29 19:29:03 -0700

    Update contact-us.tsx

--- layouts/sections/about-us/contact-us.tsx
+++ layouts/sections/about-us/contact-us.tsx
@@ -29,10 +29,7 @@ export function ContactUsSection() {
             </Text>
             <div className='space-y-2 xl:space-y-2'>
               <Text type='p' weight='thin' size='xs'>  
-                e-mail:  
-              </Text>
-              <Text type='p' weight='thin' size='xs' className='underline w-full'>  
-                <a href="mailto:jessica@soillife.org?subject=Soil Life Web Contact">jessica@soillife.org</a>          
+                e-mail:  </Text> <Text type='p' weight='thin' size='xs' color='blue' className='underline w-full'> <a href="mailto:jessica@soillife.org?subject=Soil Life Web Contact">jessica@soillife.org</a>          
               </Text>
              
                <Text type='h1' weight='regular' size='3xl' color='teal' className='mb-4 xl:mb-10'>
