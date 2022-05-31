b7bbcde2750eaf60e1e0eb17e9891cb7285ba0a4
jlchiartas <40073963+jlchiartas@users.noreply.github.com>
2022-05-30 20:30:35 -0700

    Update textural-class.tsx

--- layouts/sections/soil-101/physics/textural-class.tsx
+++ layouts/sections/soil-101/physics/textural-class.tsx
@@ -58,9 +58,9 @@ export const TexturalClassSection = () => {
         textural class
       </Text>
       <Text type='p' weight='thin' size='md' className='ml-8'>
-        soils are grouped into 12 different textural classes (based on ranges of sand, silt, and clay) that behave similarly. 
+        soils are grouped into 12 different textural classes that behave similarly, based on similar ranges of sand, silt, and clay. 
          </Text>
-      <Text type='p' weight='thin' size='xs' className='ml-8'> enter numbers (equal to 100) in the boxes below to see what the textural class will be: <br/>
+      <Text type='p' weight='thin' size='xs' className='ml-8'> enter numbers (equal to 100) in the boxes below to see what the textural class would be: <br/>
       </Text>
       <div className='sm:grid sm:grid-cols-2'>
         <div className='mb-10 sm:mb-0'>
