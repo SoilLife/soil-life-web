c8d61d9a3e14f17569cef6b3841a36f84d18afb7
jlchiartas <40073963+jlchiartas@users.noreply.github.com>
2022-05-31 06:34:44 -0700

    Update food-web.tsx

--- layouts/sections/soil-101/biology/food-web.tsx
+++ layouts/sections/soil-101/biology/food-web.tsx
@@ -135,6 +135,9 @@ export const FoodWebSection = () => {
         <Text type='h1' weight='bold' size='4xl' color='teal' className={styles['heading']}>
           soil food web
         </Text>
+        <Text type='p' weight='light' size='sm' className={`text-center ${styles['p-50']}`}>
+          soil is home to 1/4 of the world's biodiversity with billions of organisms -- 10's of 1,000's of species -- in a single teaspoon. over 90% of insects spend some portion of their life in soil. the soil food web serves as the foundation of every (terrestrial) food web on the planet!
+        </Text>
         <FoodWebSvg />
       </div>
       {modalType && (
