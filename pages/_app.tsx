6e4fa3f1341be63c9af3971fa890f4e18264fb9e
billy le <lebilly87@gmail.com>
2022-10-20 21:48:29 -0700

    google-analytics: added nextjs-ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import './_app.css';
-import 'helpers/polyfill-replaceAll';
-import type { AppProps } from 'next/app';
-import { DefaultSeo } from 'next-seo';
-import { IKContext } from 'imagekitio-react';
-import { AppProvider } from 'context';
-
-import { library } from '@fortawesome/fontawesome-svg-core';
-import * as faBrands from 'config/icon-library/fa-brands';
-import * as faSolids from 'config/icon-library/fa-solids';
+import "./_app.css";
+import "helpers/polyfill-replaceAll";
+import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
+import { IKContext } from "imagekitio-react";
+import { AppProvider } from "context";
+import { GoogleAnalytics } from "nextjs-google-analytics";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import * as faBrands from "config/icon-library/fa-brands";
+import * as faSolids from "config/icon-library/fa-solids";
 
 library.add(faSolids, faBrands);
 
@@ -15,16 +15,17 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <AppProvider>
-        <IKContext urlEndpoint='https://ik.imagekit.io/q9koofhilw/'>
-          <div className='min-h-screen antialiased'>
+        <IKContext urlEndpoint="https://ik.imagekit.io/q9koofhilw/">
+          <div className="min-h-screen antialiased">
             <DefaultSeo
               openGraph={{
-                type: 'website',
-                locale: 'en_IE',
-                url: 'https://www.soillife.org/',
-                site_name: 'Soil Life',
+                type: "website",
+                locale: "en_IE",
+                url: "https://www.soillife.org/",
+                site_name: "Soil Life",
               }}
             />
+            <GoogleAnalytics trackPageViews gaMeasurementId="G-Z3E3TD3PZ9" />
             <Component {...pageProps} />
           </div>
         </IKContext>
